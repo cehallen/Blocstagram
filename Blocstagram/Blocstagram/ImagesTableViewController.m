d942bfbdae0f7ae5ee61b7a957acdd427996253c
Chris Allen <cehallen@gmail.com>
2015-09-22 19:14:55 -0400

    rollback assignment changes

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -78,26 +78,28 @@
 
 
 
-
+/*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
     // Return NO if you do not want the specified item to be editable.
     return YES;
 }
+*/
 
 
-
+/*
 // Override to support editing the table view.
 - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
     if (editingStyle == UITableViewCellEditingStyleDelete) {
         // Delete the row from the data source
 //        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-        [self.images removeObjectAtIndex:indexPath.row]; /* didn't work without the reloadData below, vice versa.  row above is boilerplate but throws error for some reason.  when would you use it? */
+        [self.images removeObjectAtIndex:indexPath.row]; // didn't work without the reloadData below, vice versa.  row above is boilerplate but throws error for some reason.  when would you use it? 
         [tableView reloadData];
     } else if (editingStyle == UITableViewCellEditingStyleInsert) {
         // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
     }   
 }
+*/
 
 
 /*
