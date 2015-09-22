5cc842597956e5dcafd680bb9309046c1ceb5a46
Chris Allen <cehallen@gmail.com>
2015-09-21 23:42:55 -0400

    first commit, remove storyboard

---
+++ Blocstagram/Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Christopher Allen on 9/21/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Christopher Allen on 9/21/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
