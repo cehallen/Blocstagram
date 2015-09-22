a1f75a52da3f2779e36a9c9486ad38526b14e52f
Chris Allen <cehallen@gmail.com>
2015-09-22 17:30:13 -0400

    deletable rows

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Christopher Allen on 9/22/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
