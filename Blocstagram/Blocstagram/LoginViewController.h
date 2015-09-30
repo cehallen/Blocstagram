c69ada5b7d00032ff7264756e5517ad840f607de
Chris Allen <cehallen@gmail.com>
2015-09-30 11:52:59 -0400

    got into instagram

---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Christopher Allen on 9/29/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+@end
