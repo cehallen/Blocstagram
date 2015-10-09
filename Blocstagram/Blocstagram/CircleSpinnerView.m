50a183c4421139bb0d3d720c71a527673e349d97
Chris Allen <cehallen@gmail.com>
2015-10-09 09:36:49 -0400

    unfinished basic prep work

---
+++ Blocstagram/Blocstagram/CircleSpinnerView.m
@@ -0,0 +1,27 @@
+//
+//  CircleSpinnerView.m
+//  Blocstagram
+//
+//  Created by Christopher Allen on 10/9/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import "CircleSpinnerView.h"
+
+@interface CircleSpinnerView ()
+
+@property (nonatomic, strong) CAShapeLayer *circleLayer;
+
+@end
+
+@implementation CircleSpinnerView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
