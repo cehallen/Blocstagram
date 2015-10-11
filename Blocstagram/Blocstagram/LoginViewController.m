ed597cbd811a649990dfe57d1fbab2e66a0e6b11
Chris Allen <cehallen@gmail.com>
2015-10-11 14:03:38 -0400

    still not liking after reset

--- Blocstagram/Blocstagram/LoginViewController.m
+++ Blocstagram/Blocstagram/LoginViewController.m
@@ -36,6 +36,7 @@ NSString *const LoginViewControllerDidGetAccessTokenNotification = @"LoginViewCo
     self.title = NSLocalizedString(@"Login", @"Login");
     
 //    NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&redirect_uri=%@&response_type=token", [DataSource instagramClientID], [self redirectURI]];
+    // still not 'liking' properly.
     NSString *urlString = [NSString stringWithFormat:@"https://instagram.com/oauth/authorize/?client_id=%@&scope=likes+comments+relationships&redirect_uri=%@&response_type=token", [DataSource instagramClientID], [self redirectURI]];
     NSURL *url = [NSURL URLWithString:urlString];
     
