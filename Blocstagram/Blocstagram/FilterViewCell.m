34ae69542f53ebdd0b3b7fc5297396772ffc845d
Chris Allen <cehallen@gmail.com>
2015-11-05 14:03:21 -0500

    more trying

--- Blocstagram/Blocstagram/FilterViewCell.m
+++ Blocstagram/Blocstagram/FilterViewCell.m
@@ -42,7 +42,36 @@
 }
 
 
-
+//- (void)setText:(NSString *)text {
+//    static NSInteger labelTag = 1001;
+//    
+//    UILabel *label = (UILabel *)[self.contentView viewWithTag:labelTag];
+//    CGFloat thumbnailEdgeSize = self.flowLayout.itemSize.width;
+//    
+//    label = [[UILabel alloc] initWithFrame:CGRectMake(0, thumbnailEdgeSize, thumbnailEdgeSize, 20)];
+//    label.tag = labelTag;
+//    label.textAlignment = NSTextAlignmentCenter;
+//    label.font = [UIFont fontWithName:@"HelveticaNeue-Medium" size:10];
+//    [self.contentView addSubview:label];
+//    
+//    _text = text;
+//    label.text = _text;
+//}
+//
+//- (void)setImage:(UIImage *)image {
+//    static NSInteger imageViewTag = 1000;
+//    UIImageView *thumbnail = (UIImageView *)[self.contentView viewWithTag:imageViewTag];
+//    CGFloat thumbnailEdgeSize = self.flowLayout.itemSize.width;
+//    
+//    thumbnail = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, thumbnailEdgeSize, thumbnailEdgeSize)];
+//    thumbnail.contentMode = UIViewContentModeScaleAspectFill;
+//    thumbnail.tag = imageViewTag;
+//    thumbnail.clipsToBounds = YES;
+//    [self.contentView addSubview:thumbnail];
+//    
+//    _image = image;
+//    thumbnail.image = _image;
+//}
 
 
 
