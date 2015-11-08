88e5c124ba27d190a388abd3dc97467f0b63604d
Chris Allen <cehallen@gmail.com>
2015-11-08 08:32:28 -0800

    couldn't get first test working

---
+++ Blocstagram/BlocstagramTests/MediaTests.m
@@ -0,0 +1,62 @@
+//
+//  MediaTests.m
+//  Blocstagram
+//
+//  Created by Christopher Allen on 11/5/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "Media.h"
+
+@interface MediaTests : XCTestCase
+
+@end
+
+@implementation MediaTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+
+- (void)testThatInitializationWorks
+{
+    NSDictionary *sourceDictionary = @{
+                   @"id": @"1234",
+                   @"user" : @"Gabriele d'Annunzio",
+                   @"images" : @{@"standard_resolution" : @{@"url" : @"http://www.example.com/example.jpg"}},
+                   @"caption" : @{@"text" : @"Sample caption text"},
+                   @"comments" : @{@"data" : @[@"Sample comments", @"Comment 2"]},
+                   @"user_has_liked" : @YES
+               };
+    
+    Media *testMedia = [[Media alloc] initWithDictionary:sourceDictionary];
+    
+    
+    XCTAssertEqualObjects(testMedia.idNumber, sourceDictionary[@"id"], @"The ID number should be equal");
+    
+    XCTAssertEqualObjects(testMedia.user, sourceDictionary[@"user"], @"The username should be equal");
+    
+    XCTAssertEqualObjects(testMedia.mediaURL, [NSURL URLWithString:sourceDictionary[@"images"][@"standard_resolution"][@"url"]], @"The profile picture should be equal");
+    
+    XCTAssertEqualObjects(testMedia.caption, sourceDictionary[@"caption"][@"text"], @"The caption should be equal");
+    
+    XCTAssertEqualObjects(testMedia.comments, sourceDictionary[@"comments"][@"data"]);
+
+}
+
+
+
+
+
+
+
+@end
