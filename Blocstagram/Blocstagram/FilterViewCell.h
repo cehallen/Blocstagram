edfbea8a0f81449a8d727a68d869b70b56c7aeae
Chris Allen <cehallen@gmail.com>
2015-11-10 16:16:41 -0500

    setup

---
+++ Blocstagram/Blocstagram/FilterViewCell.h
@@ -0,0 +1,13 @@
+//
+//  FilterViewCell.h
+//  Blocstagram
+//
+//  Created by Christopher Allen on 11/10/15.
+//  Copyright © 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilterViewCell : UICollectionViewCell
+
+@end
